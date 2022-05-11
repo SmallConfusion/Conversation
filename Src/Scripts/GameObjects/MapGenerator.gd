e471f4fe96538c958bd79a3df9cfd33153d838f2
SmallConfusion <smallconfusion@protonmail.com>
2022-05-11 02:00:27 -0500

    Started map generation

--- Src/Scripts/GameObjects/MapGenerator.gd
+++ Src/Scripts/GameObjects/MapGenerator.gd
@@ -4,4 +4,25 @@ func _ready():
 	generate_map()
 
 func generate_map():
-	pass
+	var size = 8
+	
+	var map = []
+	
+	for i in size:
+		var row = []
+		for j in size:
+			row.append(false)
+		map.append(row)
+	
+	print_map(map)
+
+
+func print_map(map):
+	for row in map:
+		var line = ""
+		
+		for cell in row:
+			line += "X" if cell else "O"
+			line += " "
+		
+		print(line)
