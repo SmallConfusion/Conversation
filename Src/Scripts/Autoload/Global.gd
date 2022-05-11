febf349c98ad2c9965781093cdb1594d2ff474c7
SmallConfusion <smallconfusion@protonmail.com>
2022-05-10 23:08:08 -0500

    Added quit functionality

--- Src/Scripts/Autoload/Global.gd
+++ Src/Scripts/Autoload/Global.gd
@@ -4,5 +4,9 @@ extends Node
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	elif event.is_action_pressed("Fullscreen"):
+	
+	if event.is_action_pressed("Fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+	
+	if event.is_action_pressed("Quit"):
+		get_tree().quit()
